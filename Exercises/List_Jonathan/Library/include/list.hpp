485832220e48d2391cfaa937ec0d46308d35360f
Lance <lance@cybicom.com>
2021-01-19 10:11:40 +0200

    Commit my code exercise to my branch

---
+++ Exercises/List_Jonathan/Library/include/list.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+//#include <list>
+
+class nodeList
+{
+public:
+    nodeList();
+
+
+  void addItem( int item );
+
+  void removeItem( int itemValue );
+};
---
+++ Exercises/List_Jonathan/Library/src/list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "../include/list.hpp"
+
+struct Node {
+
+  int value;
+  Node* next;
+  Node* previous;
+
+};
+
+class List
+{
+  int size_;
+  Node* first_;
+  Node* last_;
+};
+
